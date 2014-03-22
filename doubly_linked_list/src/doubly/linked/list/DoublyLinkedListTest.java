bf1c0581cd88789a80ce492129c86e47592ee024
dlee0113 <dlee0113@gmail.com>
2014-03-21 21:08:20 -0400

    initial upload

---
+++ doubly_linked_list/src/doubly/linked/list/DoublyLinkedListTest.java
@@ -0,0 +1,23 @@
+package doubly.linked.list;
+
+public class DoublyLinkedListTest {
+	public static void main(String[] args) {
+		DoublyLinkedList doublyLinkedList1 = new DoublyLinkedList();
+		
+		System.out.println("addFront");
+		doublyLinkedList1.addFront(1);
+		doublyLinkedList1.addFront(2);
+		doublyLinkedList1.addFront(3);
+		doublyLinkedList1.print();
+		System.out.println();
+		
+		DoublyLinkedList doublyLinkedList2 = new DoublyLinkedList();
+		
+		System.out.println("addRear");
+		doublyLinkedList2.addRear(1);
+		doublyLinkedList2.addRear(2);
+		doublyLinkedList2.addRear(3);
+		doublyLinkedList2.print();
+		System.out.println();
+	}
+}
