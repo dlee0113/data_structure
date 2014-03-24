172d4fb64652df9bdcf88501c9d14f63ef2359cf
dlee0113 <dlee0113@gmail.com>
2014-03-23 21:36:13 -0400

    updated to test new methods

--- doubly_linked_list/src/doubly/linked/list/DoublyLinkedListTest.java
+++ doubly_linked_list/src/doubly/linked/list/DoublyLinkedListTest.java
@@ -10,6 +10,11 @@ public class DoublyLinkedListTest {
 		doublyLinkedList1.addFront(3);
 		doublyLinkedList1.print();
 		System.out.println();
+		
+		System.out.println("removeFront");
+		doublyLinkedList1.removeFront();
+		doublyLinkedList1.print();
+		System.out.println();
 
 		DoublyLinkedList doublyLinkedList2 = new DoublyLinkedList();
 
@@ -17,6 +22,17 @@ public class DoublyLinkedListTest {
 		doublyLinkedList2.addRear(1);
 		doublyLinkedList2.addRear(2);
 		doublyLinkedList2.addRear(3);
+		doublyLinkedList2.addRear(4);
+		doublyLinkedList2.print();
+		System.out.println();
+		
+		System.out.println("removeRear");
+		doublyLinkedList2.removeRear();
+		doublyLinkedList2.print();
+		System.out.println();
+		
+		System.out.println("insertAt");
+		doublyLinkedList2.insertAt(5, 2);
 		doublyLinkedList2.print();
 		System.out.println();
 	}
